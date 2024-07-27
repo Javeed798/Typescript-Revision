9aec238e93fdc317b2d61ba320513a70fd1ea19a
Javeed798 <karishmavahida@gmail.com>
2024-07-27 21:00:50 +0530

    Classes, Constructor, Getters, setters

---
+++ 11-pure-ts/dist/index.js
@@ -0,0 +1,48 @@
+"use strict";
+// console.log("Type Script Here");
+// console.log("Check Check");
+// class User {
+//   name: string;
+//   age: number;
+//   // readonly city: string = "Bangalore";
+//   private readonly city: string = "Bangalore";
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        // if i dont wanna declare the below fields i can us like public inside the constructor..
+        // name: string;
+        // age: number;
+        // readonly city: string = "Bangalore";
+        this.city = "Bangalore";
+        this._courseCount = 1;
+        this.name = name;
+        this.age = age;
+    }
+    get getAppleName() {
+        return `Apple ${this.name}`;
+    }
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    // setters wont have any return type
+    set setCourseCount(newCount) {
+        if (newCount <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = newCount;
+    }
+    // private methods
+    deleteToken() {
+        console.log("Delete Token");
+    }
+}
+const javeed = new User("javee", 23);
+// javeed.city = "Bangalore";
+javeed.setCourseCount = 5;
+console.log(javeed);
---
+++ 11-pure-ts/src/index.ts
@@ -0,0 +1,59 @@
+// console.log("Type Script Here");
+// console.log("Check Check");
+
+// class User {
+//   name: string;
+//   age: number;
+//   // readonly city: string = "Bangalore";
+//   private readonly city: string = "Bangalore";
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+class User {
+  // if i dont wanna declare the below fields i can us like public inside the constructor..
+  // name: string;
+  // age: number;
+  // readonly city: string = "Bangalore";
+  private readonly city: string = "Bangalore";
+
+  private _courseCount = 1;
+
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get getAppleName(): string {
+    return `Apple ${this.name}`;
+  }
+
+  get getCourseCount(): number {
+    return this._courseCount;
+  }
+
+  // setters wont have any return type
+  set setCourseCount(newCount: number) {
+    if (newCount <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = newCount;
+  }
+
+  // private methods
+  private deleteToken() {
+    console.log("Delete Token");
+    
+  }
+}
+
+const javeed = new User("javee", 23);
+// javeed.city = "Bangalore";
+javeed.setCourseCount = 5;
+
+console.log(javeed);
+
+
